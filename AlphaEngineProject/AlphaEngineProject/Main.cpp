b42a3584b2fe60839985ad22dccae54f236f0b43
WestMinsu <d2v210p2r@naver.com>
2025-05-23 16:47:09 +0900

    add title

--- AlphaEngineProject/AlphaEngineProject/Main.cpp
+++ AlphaEngineProject/AlphaEngineProject/Main.cpp
@@ -1,63 +1,77 @@
-// ---------------------------------------------------------------------------
-// includes
+//
+// Text rendering demo. 
+// 
+// AUTHORS: 
+//   Gerald Wong
+//  
+// DESCRIPTION:
+//   The code below shows:
+//   - How to render text
+//   - How to perform different anchoring techniques to text rendering
+//     to emulate text justification and vertical alignment.
+//
+//   Students should not take this code as a reference to
+//   "good architecture". That's not the point of this demo.
+//
 
 #include <crtdbg.h> // To check for memory leaks
 #include "AEEngine.h"
 
-
-
-// ---------------------------------------------------------------------------
-// main
-
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
-
-	int gGameRunning = 1;
-
-	// Initialization of your own variables go here
-
-	// Using custom window procedure
-	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, false, NULL);
-
-	// Changing the window title
-	AESysSetWindowTitle("My New Demo!");
-
-	// reset the system modules
-	AESysReset();
-
-	printf("Hello World\n");
-
-	// Game Loop
-	while (gGameRunning)
-	{
-		// Informing the system about the loop's start
-		AESysFrameStart();
-
-		// Basic way to trigger exiting the application
-		// when ESCAPE is hit or when the window is closed
-		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
-			gGameRunning = 0;
-
-		// Your own update logic goes here
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
 
-		// Your own rendering logic goes here
+    int gGameRunning = 1;
 
+    // Initialization of your own variables go here
 
-		// Informing the system about the loop's end
-		AESysFrameEnd();
+    // Using custom window procedure
+    AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, true, NULL);
 
-	}
+    // Changing the window title
+    AESysSetWindowTitle("Pong");
 
+    // Load the font here 
+    s8 pFont = AEGfxCreateFont("Assets/liberation-mono.ttf", 72.f);
 
-	// free the system
-	AESysExit();
+    // Text to print
+    const char* pText = "Press space key to start...";
+
+    f32 w, h;
+    AEGfxGetPrintSize(pFont, pText, 1.f, &w, &h);
+
+    // Game Loop
+    while (gGameRunning)
+    {
+        AESysFrameStart();
+        AEGfxSetBackgroundColor(0.1f, 0.1f, 0.1f);
+
+        //
+        // Text that is drawn in the middle, as if it has:
+        // - Center justification
+        // - Center vertical alignment
+        //
+        AEGfxPrint(pFont, pText, -w / 2, -h / 2, 1, 1, 1, 1, 1);
+
+        // Informing the system about the loop's end
+        AESysFrameEnd();
+
+        // check if forcing the application to quit
+        if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
+            gGameRunning = 0;
+    }
+
+    AEGfxDestroyFont(pFont);
+
+    AESysExit();
 }
+
+
+
