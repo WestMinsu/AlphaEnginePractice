3b5ef60199061ac2e8fb88ba8504e4a5fb1abec3
WestMinsu <d2v210p2r@naver.com>
2025-06-04 21:39:40 +0900

    implement pong button

--- AlphaEngineProject/AlphaEngineProject/MainMenuState.cpp
+++ AlphaEngineProject/AlphaEngineProject/MainMenuState.cpp
@@ -2,6 +2,17 @@
 #include "GameManager.h"
 #include <string>
 #include "Constants.h"
+#include <iostream>
+
+const f32 buttonWidth = 350.f;
+const f32 buttonHeight = 100.f;
+
+const f32 pongButtonX = 0.f;
+const f32 pongButtonY = 200.f;
+
+const f32 animationButtonX = 0.f;
+const f32 animationButtonY = -200.f;
+
 
 void MainMenuState::Enter(GameManager* gameManager)
 {
@@ -9,21 +20,20 @@ void MainMenuState::Enter(GameManager* gameManager)
 
 void MainMenuState::Update(GameManager* gameManager, f32 dt)
 {
-	if (AEInputCheckReleased(AEVK_LBUTTON)) // && click button
-		gameManager->ChangeState(GameState::MAIN_GAME);
+	s32 pX, pY;
+	AEInputGetCursorPosition(&pX, &pY);
+	pX -= 800;
+	pY = 450 - pY;
+	std::cout << "x = " << pX << "y = " << pY << std::endl;
+	if (AEInputCheckReleased(AEVK_LBUTTON))
+	{
+		if ((pongButtonX - buttonWidth / 2 <= pX && pX <= pongButtonY + buttonWidth / 2) && (pongButtonY - buttonHeight / 2 <= pY && pY <= pongButtonY + buttonHeight / 2))
+			gameManager->ChangeState(GameState::MAIN_GAME);
+	}
 }
 
 void MainMenuState::Draw(GameManager* gameManager)
 {
-	f32 buttonWidth = 350.f;
-	f32 buttonHeight = 100.f;
-
-	f32 pongButtonX = 0.f;
-	f32 pongButtonY = 200.f;
-
-	f32 animationButtonX = 0.f;
-	f32 animationButtonY = -200.f;
-
 	AEGfxSetBackgroundColor(0.1f, 0.1f, 0.1f);
 	gameManager->DrawRect(pongButtonX, pongButtonY, buttonWidth, buttonHeight, 0.5, 0.5, 0.5, 1);
 	gameManager->DrawRect(animationButtonX, animationButtonY, buttonWidth, buttonHeight, 0.5, 0.5, 0.5, 1);
@@ -32,16 +42,16 @@ void MainMenuState::Draw(GameManager* gameManager)
 	std::string animationButtonText = "Animation";
 	f32 TextScale = 0.9f;
 
-	f32 w, h;
-	AEGfxGetPrintSize(gameManager->m_font, pongButtonText.c_str(), TextScale, &w, &h);
-	f32 w2, h2;
-	AEGfxGetPrintSize(gameManager->m_font, animationButtonText.c_str(), TextScale, &w2, &h2);
+	f32 pongButtonTextWidth, pongButtonTextHeight;
+	AEGfxGetPrintSize(gameManager->m_font, pongButtonText.c_str(), TextScale, &pongButtonTextWidth, &pongButtonTextHeight);
+	f32 animationButtonTextWidth, animationButtonTextHeight;
+	AEGfxGetPrintSize(gameManager->m_font, animationButtonText.c_str(), TextScale, &animationButtonTextWidth, &animationButtonTextHeight);
 
-	f32 pongTextX = 2 * pongButtonX / kGameWindowWidth - w / 2;
-	f32 pongtextY = 2 * pongButtonY / kGameWindowHeight - h / 2;
+	f32 pongTextX = 2 * pongButtonX / kGameWindowWidth - pongButtonTextWidth / 2;
+	f32 pongtextY = 2 * pongButtonY / kGameWindowHeight - pongButtonTextHeight / 2;
 
-	f32 animationTextX = 2 * animationButtonX / kGameWindowWidth - w2 / 2;
-	f32 animationTextY = 2 * animationButtonY / kGameWindowHeight - h2 / 2;
+	f32 animationTextX = 2 * animationButtonX / kGameWindowWidth - animationButtonTextWidth / 2;
+	f32 animationTextY = 2 * animationButtonY / kGameWindowHeight - animationButtonTextHeight / 2;
 
 	AEGfxPrint(gameManager->m_font, pongButtonText.c_str(), pongTextX, pongtextY, TextScale, 1, 1, 1, 1);
 	AEGfxPrint(gameManager->m_font, animationButtonText.c_str(), animationTextX, animationTextY, TextScale, 1, 1, 1, 1);
